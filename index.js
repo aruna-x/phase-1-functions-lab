288639c66c7b224decdb7553dab94d9fb2f0d968
Aruna <aruna.evan15@gmail.com>
2021-09-21 09:45:14 -0700

    commit tree 3dea763d5879a36bf08811f32632d62f12a56691

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(b) {
+    return Math.abs(42-b)
+}
+
+function distanceFromHqInFeet(b) {
+    return distanceFromHqInBlocks(b)*264
+}
+
+function distanceTravelledInFeet(b1, b2) {
+    return Math.abs(b1-b2)*264
+}
+
+function calculatesFarePrice(start, destination) {
+    let dist = distanceTravelledInFeet(start, destination);
+    if (dist <= 400) {
+        return 0;
+    } else if (dist>400 && dist<=2000 ) {
+        return (dist-400)*2/100;
+    } else if (dist>2000 && dist<2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+
+}
